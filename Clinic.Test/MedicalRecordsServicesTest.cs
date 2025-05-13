b0dc93b843f9055d423fb2132a2f8f46f27bbf02
yassin <yassinbenyahia17@gmail.com>
2025-05-12 22:08:56 -0400

    Add Unit Test for Payment and MedicalRecord

---
+++ Clinic.Test/MedicalRecordsServicesTest.cs
@@ -0,0 +1,131 @@
+﻿using Xunit;
+using Moq;
+using System;
+using System.Collections.Generic;
+using DataLayer.Repositories.Interfaces;
+
+namespace Clinic.Tests.Services
+{
+    public class MedicalRecordsServiceTests
+    {
+        private readonly Mock<IMedicalRecordRepository> _mockRepo;
+        private readonly MedicalRecordsService _service;
+
+        public MedicalRecordsServiceTests()
+        {
+            _mockRepo = new Mock<IMedicalRecordRepository>();
+            _service = new MedicalRecordsService(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void AddMedicalRecord_WithNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _service.AddMedicalRecord(null));
+        }
+
+        [Fact]
+        public void AddMedicalRecord_AlreadyExists_ThrowsArgumentException()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(true);
+
+            var ex = Assert.Throws<ArgumentException>(() => _service.AddMedicalRecord(record));
+            Assert.Equal("Medical record already exists", ex.Message);
+        }
+
+        [Fact]
+        public void AddMedicalRecord_Valid_ReturnsId()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(false);
+            _mockRepo.Setup(r => r.Add(record)).Returns(1);
+
+            var result = _service.AddMedicalRecord(record);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void DeleteMedicalRecord_WithNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _service.DeleteMedicalRecord(null));
+        }
+
+        [Fact]
+        public void DeleteMedicalRecord_NotExists_ThrowsArgumentException()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(false);
+
+            var ex = Assert.Throws<ArgumentException>(() => _service.DeleteMedicalRecord(record));
+            Assert.Equal("Medical record does not exist", ex.Message);
+        }
+
+        [Fact]
+        public void DeleteMedicalRecord_Valid_ReturnsTrue()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(true);
+            _mockRepo.Setup(r => r.Delete(record)).Returns(true);
+
+            var result = _service.DeleteMedicalRecord(record);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetMedicalRecordByID_NegativeId_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _service.GetMedicalRecordByID(-1));
+        }
+
+        [Fact]
+        public void GetMedicalRecordByID_NotFound_ThrowsKeyNotFoundException()
+        {
+            _mockRepo.Setup(r => r.GetByID(1)).Returns((MedicalRecord)null);
+
+            var ex = Assert.Throws<KeyNotFoundException>(() => _service.GetMedicalRecordByID(1));
+            Assert.Equal("Medical record with ID 1 not found", ex.Message);
+        }
+
+        [Fact]
+        public void GetMedicalRecordByID_Valid_ReturnsRecord()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.GetByID(1)).Returns(record);
+
+            var result = _service.GetMedicalRecordByID(1);
+
+            Assert.NotNull(result);
+            Assert.Equal(1, result.MedicalRecordID);
+        }
+
+        [Fact]
+        public void UpdateMedicalRecord_WithNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _service.UpdateMedicalRecord(null));
+        }
+
+        [Fact]
+        public void UpdateMedicalRecord_NotExists_ThrowsArgumentException()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(false);
+
+            var ex = Assert.Throws<ArgumentException>(() => _service.UpdateMedicalRecord(record));
+            Assert.Equal("Medical record does not exist", ex.Message);
+        }
+
+        [Fact]
+        public void UpdateMedicalRecord_Valid_ReturnsTrue()
+        {
+            var record = new MedicalRecord { MedicalRecordID = 1 };
+            _mockRepo.Setup(r => r.DoesExist(1)).Returns(true);
+            _mockRepo.Setup(r => r.Update(record)).Returns(true);
+
+            var result = _service.UpdateMedicalRecord(record);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ Clinic.Test/PaymentServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using DataLayer.Repositories.Interfaces;
+
+namespace Clinic.Tests.Services
+{
+    public class PaymentServiceTests
+    {
+        private readonly Mock<IPaymentRepository> _mockRepo;
+        private readonly PaymentService _service;
+
+        public PaymentServiceTests()
+        {
+            _mockRepo = new Mock<IPaymentRepository>();
+            _service = new PaymentService(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void AddPayment_ShouldReturnId_WhenSuccessful()
+        {
+            var payment = new Payment { PaymentID = 1, AmountPaid  = 100, PaymentDate = DateTime.Today };
+
+            _mockRepo.Setup(r => r.Add(payment)).Returns(1);
+
+            var result = _service.AddPayment(payment);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void AddPayment_ShouldThrow_WhenPaymentIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _service.AddPayment(null));
+        }
+
+        [Fact]
+        public void DeletePayment_ShouldReturnTrue_WhenSuccessful()
+        {
+            var payment = new Payment { PaymentID = 1 };
+            _mockRepo.Setup(r => r.GetByID(1)).Returns(payment);
+            _mockRepo.Setup(r => r.Delete(payment)).Returns(true);
+
+            var result = _service.DeletePayment(1);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void DeletePayment_ShouldThrow_WhenPaymentNotFound()
+        {
+            _mockRepo.Setup(r => r.GetByID(1)).Returns((Payment)null);
+
+            Assert.Throws<KeyNotFoundException>(() => _service.DeletePayment(1));
+        }
+
+        [Fact]
+        public void GetPaymentById_ShouldReturnPayment_WhenFound()
+        {
+            var payment = new Payment { PaymentID = 1 };
+            _mockRepo.Setup(r => r.GetByID(1)).Returns(payment);
+
+            var result = _service.GetPaymentById(1);
+
+            Assert.Equal(1, result.PaymentID);
+        }
+
+        [Fact]
+        public void UpdatePayment_ShouldReturnTrue_WhenSuccessful()
+        {
+            var payment = new Payment { PaymentID = 1 };
+            _mockRepo.Setup(r => r.Update(payment)).Returns(true);
+
+            var result = _service.UpdatePayment(payment);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetPaymentsByPatient_ShouldReturnResults()
+        {
+            var payments = new List<Payment> { new Payment { Appointments = new Appointments { PatientID = 1 } } }.AsQueryable();
+            _mockRepo.Setup(r => r.GetPaymentsByPatient(1)).Returns(payments);
+
+            var result = _service.GetPaymentsByPatient(1);
+
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void GetPaymentsByDateRange_ShouldThrow_WhenInvalidRange()
+        {
+            var from = DateTime.Today;
+            var to = DateTime.Today.AddDays(-1); // Invalid
+
+            Assert.Throws<ArgumentException>(() => _service.GetPaymentsByDateRange(from, to));
+        }
+    }
+}
+
